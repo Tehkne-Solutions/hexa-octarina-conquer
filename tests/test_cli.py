f163e22c4b2808a59edfe4bc10a8a210e78b4e6a
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-17 21:09:25 -0300

    test: update CLI command coverage

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,11 +8,11 @@ from hexa_octarina_conquer.cli import parse_command
 
 
 class CliTests(unittest.TestCase):
-    def test_parse_command_for_edge_action(self):
+    def test_parse_edge(self):
         self.assertEqual(parse_command("edge 0 0 1 0"), ("edge", ((0, 0), (1, 0))))
 
-    def test_parse_command_for_state_action(self):
-        self.assertEqual(parse_command("state"), ("state", ()))
+    def test_parse_duels(self):
+        self.assertEqual(parse_command("duels"), ("duels", ()))
 
 
 if __name__ == "__main__":
