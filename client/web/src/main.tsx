cfb7502a8dda3ba206afa26a2468fda44df22e46
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-27 19:19:41 -0300

    style: ativar estruturas estratégicas premium

--- client/web/src/main.tsx
+++ client/web/src/main.tsx
@@ -51,6 +51,7 @@ import "./pack99-premium-campaign.css";
 import "./pack99-premium-hud.css";
 import "./pack99-premium-cards.css";
 import "./pack99-territory-minimap.css";
+import "./pack99-strategic-structures.css";
 
 const LegacyApp = lazy(() => import("./App").then((module) => ({ default: module.App })));
 const SprintUi08VisualQa = lazy(() => import("./SprintUi08VisualQa").then((module) => ({ default: module.SprintUi08VisualQa })));
