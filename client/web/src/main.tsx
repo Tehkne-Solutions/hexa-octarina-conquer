a71483c43c9cb1a53a16b9cc4d93514bb6089ab4
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-07-27 18:27:36 -0300

    feat: ativar composição premium da campanha PACK 99

--- client/web/src/main.tsx
+++ client/web/src/main.tsx
@@ -46,6 +46,7 @@ import "./pack99-unit-sprite.css";
 import "./pack99-unit-motion.css";
 import "./pack99-living-world.css";
 import "./pack99-world-vfx.css";
+import "./pack99-premium-campaign.css";
 
 const LegacyApp = lazy(() => import("./App").then((module) => ({ default: module.App })));
 const SprintUi08VisualQa = lazy(() => import("./SprintUi08VisualQa").then((module) => ({ default: module.SprintUi08VisualQa })));
