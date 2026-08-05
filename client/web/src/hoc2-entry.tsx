dddf72e157f5feb5cf760b5fc753359aec5fc543
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-08-05 15:31:13 -0300

    HOC2 VS01-E: load strategic network styles

--- client/web/src/hoc2-entry.tsx
+++ client/web/src/hoc2-entry.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import { Hoc2Game } from "./hoc2/Hoc2Game";
+import "./hoc2/hoc2-network.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("root element was not found");
