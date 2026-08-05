559c88dd9f7c2ae0df832b0928943a38d14de929
Thales Wallison (TW Da Vinci) <thales.dvf@gmail.com>
2026-08-05 18:48:57 -0300

    HOC2 VS01-K: load visual remediation overrides

--- client/web/src/hoc2-entry.tsx
+++ client/web/src/hoc2-entry.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 
 import { Hoc2Game } from "./hoc2/Hoc2Game";
 import "./hoc2/hoc2-network.css";
+import "./hoc2/hoc2-remediation.css";
 
 const root = document.getElementById("root");
 if (!root) throw new Error("root element was not found");
